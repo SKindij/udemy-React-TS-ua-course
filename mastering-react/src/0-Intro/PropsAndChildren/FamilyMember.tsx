44f1f4aa4ec9309c44aa4066b57ff2473a9e2148
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-06 16:43:56 +0300

    init: set up project structure and dependencies

---
+++ mastering-react/src/0-Intro/PropsAndChildren/FamilyMember.tsx
@@ -0,0 +1,19 @@
+import { ColoredBlock } from 'components';
+import { ReactNode } from 'react';
+
+import { Avatar } from './Avatar';
+import classes from './FamilyMember.module.css';
+
+interface Props {
+  name:string;
+  children?:ReactNode;
+}
+
+export function FamilyMember({ name, children }:Props):JSX.Element {
+  return (
+    <ColoredBlock style={{ flex: 1 }}>
+      <Avatar name={name} />
+      {children ? <div className={classes.children}>{children}</div> : null}
+    </ColoredBlock>
+  );
+}
---
+++ mastering-react/src/components/index.tsx
@@ -0,0 +1,39 @@
+import { Button } from './Button/Button';
+import { ChapterWrapper } from './ChapterWrapper/ChapterWrapper';
+import { ColoredBlock } from './ColoredBlock/ColoredBlock';
+import { EmojiButton } from './EmojiButton/EmojiButton';
+import { FlexFiller } from './FlexFiller';
+import { FlexRow } from './FlexRow/FlexRow';
+import { Label } from './Label/Label';
+import { LoadingSpinner } from './LoadingSpinner/LoadingSpinner';
+import { LoggedLifecycle } from './LoggedLifecycle';
+import { Chapter, MasterDetail, Section } from './MasterDetail';
+import { PropsTable } from './PropsTable/PropsTable';
+import { Switch } from './Switch/Switch';
+import { Tabs } from './Tabs/Tabs';
+import { TabsHeader } from './Tabs/TabsHeader';
+import { TextBlock } from './TextBlock/TextBlock';
+import { Toolbar } from './Toolbar/Toolbar';
+import { ValueLabel } from './ValueLabel/ValueLabel';
+
+export {
+  Button,
+  Chapter,
+  ChapterWrapper,
+  ColoredBlock,
+  EmojiButton,
+  FlexFiller,
+  FlexRow,
+  Label,
+  LoadingSpinner,
+  LoggedLifecycle,
+  MasterDetail,
+  PropsTable,
+  Section,
+  Switch,
+  Tabs,
+  TabsHeader,
+  TextBlock,
+  Toolbar,
+  ValueLabel,
+};
