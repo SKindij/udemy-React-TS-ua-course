64de80638c947327222f4744a7f718810df23a43
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-06 18:37:32 +0300

    init: set up project structure and dependencies

---
+++ mastering-react/src/0-Intro/JSX/Fragments.tsx
@@ -0,0 +1,38 @@
+import { ChapterWrapper } from 'components';
+import { Fragment } from 'react';
+
+function Fruits():JSX.Element {
+  return (
+    <Fragment>
+      <li>apple 🍎</li>
+      <li>pear 🍐</li>
+      <li>orange 🍊</li>
+      <li>banana 🍌</li>
+      <li>watermelon 🍉</li>
+    </Fragment>
+  );
+}
+
+function Vegetables():JSX.Element {
+  return (
+    <>
+      <li>tomato 🍅</li>
+      <li>eggplant 🍆</li>
+      <li>avocado 🥑</li>
+      <li>cucumber 🥒</li>
+      <li>pepper 🌶</li>
+    </>
+  );
+}
+
+export function Fragments():JSX.Element {
+  return (
+    <ChapterWrapper title="Fragments" subtitle="Introduction to JSX">
+      List of fruits and vegetables:
+      <ul>
+        <Fruits />
+        <Vegetables />
+      </ul>
+    </ChapterWrapper>
+  );
+}
