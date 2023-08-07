5edbb53caf6d29fb5567d8c7acfcaffa2d8b25fa
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-07 21:22:12 +0300

    init: create an additional file

---
+++ mastering-react/src/0-Intro/JSX/ReactWithoutJsx.tsx
@@ -0,0 +1,25 @@
+import { ChapterWrapper } from 'components';
+import { createElement } from 'react';
+
+export function ReactWithoutJsx() {
+  const props = {
+    title: 'React without JSX',
+    subtitle: 'Introduction to JSX',
+    children: createElement(
+      'p',
+      { style: { fontSize: '18px' } },
+      `You don't have to use JSX, but the code is much harder to work without it.`
+    ),
+  };
+  return createElement(ChapterWrapper, props);
+}
+
+export function SameThingWithJsx():JSX.Element {
+  return (
+    <ChapterWrapper title="Same thing with JSX" subtitle="Introduction to JSX">
+      <p style={{ fontSize: '18px' }}>
+        You don't have to use JSX, but the code is much harder to read without it.
+      </p>
+    </ChapterWrapper>
+  );
+}
