40e1b85fb29e5586e21d427758552747651c23ea
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-13 07:46:42 +0300

    init: create an additional file

---
+++ mastering-react/src/1-HooksBasics/UseStateBasics.tsx
@@ -0,0 +1,48 @@
+import { Button, ChapterWrapper, Toolbar, ValueLabel } from 'components';
+import { ChangeEventHandler, useState } from 'react';
+
+const FIBONACCI = [1, 1];
+
+export function UseStateBasics(): JSX.Element {
+  const [clicks, setClicks] = useState<number>(0);
+  const [isClicksVisible, setClicksVisible] = useState<boolean>(true);
+  const [someText, setSomeText] = useState<string>('');
+  const [fibonacci, setFibonacci] = useState<number[]>(FIBONACCI);
+
+  const incrementCounter = () => setClicks(clicks + 1);
+  const showClicks = () => setClicksVisible(true);
+  const hideClicks = () => setClicksVisible(false);
+
+  const onInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setSomeText(event.target.value);
+  };
+  const clearText = () => setSomeText('');
+
+  const addFibonacci = () => {
+    const copy = [...fibonacci];
+    const lastIdx = copy.length - 1;
+    copy.push(copy[lastIdx] + copy[lastIdx - 1]);
+    setFibonacci(copy);
+  };
+
+  return (
+    <ChapterWrapper title="What is a hook?" subtitle="Hooks basics, useState">
+      <Toolbar>
+        <ValueLabel value={isClicksVisible ? clicks : '?'} minWidth={100} />
+        <Button onClick={incrementCounter} text="Increment counter" />
+        <Button onClick={showClicks} text="Show clicks" disabled={isClicksVisible} />
+        <Button onClick={hideClicks} text="Hide clicks" disabled={!isClicksVisible} />
+      </Toolbar>
+
+      <Toolbar>
+        <input type="text" value={someText} onChange={onInputChange} />
+        <Button text="Clear" onClick={clearText} />
+      </Toolbar>
+
+      <Toolbar>
+        <Button text="Add Fibonacci number" onClick={addFibonacci} />
+      </Toolbar>
+      <div>Fibonacci sequence: {fibonacci.join(', ')}</div>
+    </ChapterWrapper>
+  );
+}
