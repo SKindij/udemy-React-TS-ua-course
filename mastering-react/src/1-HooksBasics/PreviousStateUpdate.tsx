ccf751cc05b2ec5d8d0fde682fd3149542630217
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-13 07:42:05 +0300

    init: add necessary file

---
+++ mastering-react/src/1-HooksBasics/PreviousStateUpdate.tsx
@@ -0,0 +1,48 @@
+import { Button, ChapterWrapper, Toolbar, ValueLabel } from 'components';
+import { useState } from 'react';
+
+const DELAY = 2000;
+const FIBONACCI = [1, 1];
+
+export function PreviousStateUpdate(): JSX.Element {
+  const [clicks, setClicks] = useState<number>(0);
+  const [isClicksVisible, setClicksVisible] = useState<boolean>(true);
+  const [fibonacci, setFibonacci] = useState<number[]>(FIBONACCI);
+
+  const incrementCounter = () => {
+    setTimeout(() => setClicks(clicks + 1), DELAY);
+  };
+
+  const incrementWithDelay = () => {
+    setTimeout(() => setClicks((prevClicks) => prevClicks + 1), DELAY);
+  };
+
+  const toggleShowClicks = () => {
+    setClicksVisible((prevValue) => !prevValue);
+  };
+
+  const addFibonacci = () => {
+    setFibonacci((array) => {
+      const copy = [...array];
+      const lastIdx = copy.length - 1;
+      copy.push(copy[lastIdx] + copy[lastIdx - 1]);
+      return copy;
+    });
+  };
+
+  return (
+    <ChapterWrapper title="Previous state update" subtitle="Hooks basics, useState">
+      <Toolbar>
+        <ValueLabel value={isClicksVisible ? clicks : '?'} minWidth={100} />
+        <Button onClick={incrementCounter} text="Increment counter" />
+        <Button onClick={incrementWithDelay} text="Increment with delay" />
+        <Button onClick={toggleShowClicks} text="Toggle show clicks" />
+      </Toolbar>
+
+      <Toolbar>
+        <Button text="Add Fibonacci number" onClick={addFibonacci} />
+      </Toolbar>
+      <div>Fibonacci sequence: {fibonacci.join(', ')}</div>
+    </ChapterWrapper>
+  );
+}
