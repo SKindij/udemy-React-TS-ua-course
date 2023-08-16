725f56ac99b48eb3187c8c1a0e691c95d249c209
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-16 15:39:17 +0300

    init: set up project structure

---
+++ mastering-react/src/2-SideEffects/UseGlobalEvents/UseGlobalEvents.tsx
@@ -0,0 +1,36 @@
+import { ChapterWrapper, Toolbar } from 'components';
+import { useEffect, useState } from 'react';
+
+import { WindowSizePreview } from './WindowSizePreview';
+
+const STEP = 10;
+const MIN_WIDTH = 100;
+const MAX_WIDTH = 300;
+
+export function UseGlobalEvents(): JSX.Element {
+  const [previewWidth, setPreviewWidth] = useState<number>((MAX_WIDTH + MIN_WIDTH) / 2);
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          setPreviewWidth((value) => Math.max(value - STEP, MIN_WIDTH));
+          break;
+        case 'ArrowRight':
+          setPreviewWidth((value) => Math.min(value + STEP, MAX_WIDTH));
+          break;
+      }
+    };
+    document.addEventListener('keydown', onKeyDown);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, []);
+
+  return (
+    <ChapterWrapper title="Listening to global events" subtitle="Side effects, useEffect">
+      <Toolbar>Use ⬅️ ➡️ to change the preview window size.</Toolbar>
+      <WindowSizePreview previewWidth={previewWidth} />
+    </ChapterWrapper>
+  );
+}
