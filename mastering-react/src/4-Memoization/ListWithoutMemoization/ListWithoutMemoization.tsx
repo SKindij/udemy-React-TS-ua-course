53504913c93969ef2a69e4dc32fa0609c859a1da
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-19 00:05:08 +0300

    init: set up project structure and dependencies

---
+++ mastering-react/src/4-Memoization/ListWithoutMemoization/ListWithoutMemoization.tsx
@@ -0,0 +1,17 @@
+import { ChapterWrapper } from 'components';
+import { useRerender } from 'hooks/useRerender';
+
+import { ITEMS } from '../ItemData';
+import { EmojisGallery } from './EmojisGallery';
+
+export function ListWithoutMemoization() {
+  return (
+    <ChapterWrapper
+      title="useCallback"
+      subtitle="List without memoization"
+      rerender={useRerender()}
+    >
+      <EmojisGallery items={ITEMS} />
+    </ChapterWrapper>
+  );
+}
