15752a3cd4353740fd94a1d57c06bd0ebfcef0f5
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-19 16:25:54 +0300

    init: set up project structure and dependencies

---
+++ mastering-react/src/3-ImperativeCode/StoringStateInUseRef/StoringStateInUseRef.tsx
@@ -0,0 +1,39 @@
+import { useCounter } from '1-HooksBasics/CustomHooks/useCounter';
+import { Button, ChapterWrapper, Toolbar, ValueLabel } from 'components';
+import { useLoggedLifecycle } from 'hooks/useLoggedLifecycle';
+import { logTagged } from 'utils/logTagged';
+
+import { useRefState } from './useRefState';
+import { useUpdateEffect } from './useUpdateEffect';
+
+export function StoringStateInUseRef(): JSX.Element {
+  const { value: stateClicks, increase: increaseStateClicks } = useCounter();
+  const [refStateClicks, setRefStateClicks] = useRefState<number>(0);
+
+  useLoggedLifecycle('Main');
+
+  useUpdateEffect(() => {
+    logTagged('useUpdateEffect', `value: ${stateClicks}`);
+  }, [stateClicks]);
+
+  const increaseRefStateClicks = () => {
+    setRefStateClicks((value) => value + 1);
+    logTagged('useRefState', 'increase ref state');
+  };
+
+  return (
+    <ChapterWrapper title="Storing state in useRef" subtitle="useUpdateEffect">
+      <div>useState clicks:</div>
+      <Toolbar>
+        <ValueLabel value={stateClicks} minWidth="100px" />
+        <Button text="+1" onClick={increaseStateClicks} />
+      </Toolbar>
+
+      <div>useRefState clicks:</div>
+      <Toolbar>
+        <ValueLabel value={refStateClicks} minWidth="100px" />
+        <Button text="+1" onClick={increaseRefStateClicks} />
+      </Toolbar>
+    </ChapterWrapper>
+  );
+}
