29e867ae55f6ded10a6345c0599dc43a2922f557
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-19 16:14:39 +0300

    feat: implement new module

---
+++ mastering-react/src/3-ImperativeCode/ImperativeHandle/UseImperativeHandle.tsx
@@ -0,0 +1,32 @@
+import { Button, ChapterWrapper, Toolbar } from 'components';
+import { useRef, useState } from 'react';
+
+import { PinInput, PinInputHandle } from './PinInput';
+
+const initialDigits = ['', '', '', ''];
+
+export function UseImperativeHandle(): JSX.Element {
+  const [digits, setDigits] = useState<string[]>(initialDigits);
+  const ref = useRef<PinInputHandle | null>(null);
+
+  const focus = () => {
+    ref.current?.focus();
+  };
+
+  const clear = () => {
+    setDigits(initialDigits);
+  };
+
+  return (
+    <ChapterWrapper title="useImperativeHandle" subtitle="PinInput component">
+      <Toolbar>
+        <PinInput ref={ref} digits={digits} onChange={setDigits} />
+      </Toolbar>
+
+      <Toolbar>
+        <Button text="Focus" onClick={focus} />
+        <Button text="Clear" onClick={clear} />
+      </Toolbar>
+    </ChapterWrapper>
+  );
+}
