2169b66724f6860692df6d63a6cc506c851598d3
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-19 16:17:50 +0300

    feat: implement new module

---
+++ mastering-react/src/3-ImperativeCode/RockScissorsPaper/RockScissorsPaper.tsx
@@ -0,0 +1,22 @@
+import { ChapterWrapper, Toolbar } from 'components';
+import { useState } from 'react';
+
+import { HandShape } from './HandShape';
+import { HandShapeEmoji } from './HandShapeEmoji';
+import { RockScissorsPaperInput } from './RockScissorsPaperInput';
+
+export function RockScissorsPaper(): JSX.Element {
+  const [currentValue, setCurrentValue] = useState<HandShape>('✊');
+
+  return (
+    <ChapterWrapper title="Rock Scissors Paper">
+      <Toolbar>
+        <RockScissorsPaperInput currentValue={currentValue} onChange={setCurrentValue} />
+      </Toolbar>
+
+      <Toolbar>
+        Current value: <HandShapeEmoji value={currentValue} />
+      </Toolbar>
+    </ChapterWrapper>
+  );
+}
