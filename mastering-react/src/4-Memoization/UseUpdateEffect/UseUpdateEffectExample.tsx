880aa55b5c2c36fc0b842cd6aaa54471e90fb740
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-20 14:05:28 +0300

    init: set up project structure

---
+++ mastering-react/src/4-Memoization/UseUpdateEffect/UseUpdateEffectExample.tsx
@@ -0,0 +1,32 @@
+import { useCounter } from '1-HooksBasics/CustomHooks/useCounter';
+import { Button, ChapterWrapper, Toolbar, ValueLabel } from 'components';
+import { useLoggedLifecycle } from 'hooks/useLoggedLifecycle';
+import { useCallback, useEffect } from 'react';
+import { logTagged } from 'utils/logTagged';
+
+import { useUpdateEffect } from './useUpdateEffect';
+
+export function UseUpdateEffect(): JSX.Element {
+  const { value, increase } = useCounter();
+
+  useLoggedLifecycle('Parent');
+
+  useEffect(() => {
+    logTagged('useEffect', 'effect with empty deps list');
+  }, []);
+
+  useUpdateEffect(
+    useCallback(() => {
+      logTagged('updateEffect', `value === ${value}`);
+    }, [value])
+  );
+
+  return (
+    <ChapterWrapper title="useCallback" subtitle="useUpdateEffect">
+      <Toolbar>
+        <ValueLabel value={value} minWidth="100px" />
+        <Button text="+1" onClick={increase} />
+      </Toolbar>
+    </ChapterWrapper>
+  );
+}
