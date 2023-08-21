e3748803fb9c54f4947054fcd031b1d742dd96c9
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-21 14:27:37 +0300

    init: add necessary file

---
+++ mastering-react/src/4-Memoization/UseMemo/UseMemoExample.tsx
@@ -0,0 +1,44 @@
+import { ChapterWrapper, ColoredBlock } from 'components';
+import { useRerender } from 'hooks/useRerender';
+import { memo, MouseEventHandler, useMemo } from 'react';
+
+import { Leaf } from './Leaf';
+import { nodeStyle } from './nodeStyle';
+
+interface NodeProps {
+  level: number;
+  maxLevel: number;
+  path: string[];
+}
+
+const MemoizedNode = memo(({ level, maxLevel, path }: NodeProps): JSX.Element => {
+  const rerender = useRerender();
+  const leftPath = useMemo(() => [...path, 'left'], [path]);
+  const rightPath = useMemo(() => [...path, 'right'], [path]);
+
+  const handleClick: MouseEventHandler = (event) => {
+    event.stopPropagation();
+    rerender();
+  };
+
+  if (level === maxLevel) {
+    return <Leaf path={path} onClick={handleClick} />;
+  }
+
+  return (
+    <ColoredBlock style={nodeStyle} onClick={handleClick}>
+      <MemoizedNode level={level + 1} maxLevel={maxLevel} path={leftPath} />
+      <MemoizedNode level={level + 1} maxLevel={maxLevel} path={rightPath} />
+    </ColoredBlock>
+  );
+});
+
+const ROOT_PATH = ['root'];
+
+export function UseMemoExample(): JSX.Element {
+  return (
+    <ChapterWrapper title="useMemo" subtitle="React.memo + useMemo" rerender={useRerender()}>
+      <MemoizedNode level={0} maxLevel={2} path={ROOT_PATH} />
+    </ChapterWrapper>
+  );
+}
