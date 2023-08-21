f0279795142963f8678732ae84e7e82c95cf0c26
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-21 21:41:47 +0300

    init: add necessary file

---
+++ mastering-react/src/5-ComponentsLifecycle/RandomRendering/RandomRendering.tsx
@@ -0,0 +1,31 @@
+import { ChapterWrapper, TextBlock } from 'components';
+import { useRerender } from 'hooks/useRerender';
+
+import { Child } from './Child';
+import { RandomContainer } from './RandomContainer';
+
+const EMOJIS = ['🏍', '✈️', '🚀', '🏎'];
+
+export function RandomRendering(): JSX.Element {
+  return (
+    <ChapterWrapper
+      title="Random container"
+      subtitle="Components lifecycle"
+      rerender={useRerender()}
+    >
+      <TextBlock>
+        <div>
+          ℹ️ If you pass a child component to some custom container component, there is no guarantee
+          it'll be rendered.
+        </div>
+        <div>ℹ️ It fully depends on the rendering logic of container component.</div>
+      </TextBlock>
+
+      <RandomContainer>
+        {EMOJIS.map((emoji) => (
+          <Child key={emoji} tag={emoji} />
+        ))}
+      </RandomContainer>
+    </ChapterWrapper>
+  );
+}
