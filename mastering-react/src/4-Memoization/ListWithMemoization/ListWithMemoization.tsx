882bf514fb1106a80518503d5514e8dba0fc0abc
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-21 12:52:32 +0300

    init: add necessary file

---
+++ mastering-react/src/4-Memoization/ListWithMemoization/ListWithMemoization.tsx
@@ -0,0 +1,13 @@
+import { ChapterWrapper } from 'components';
+import { useRerender } from 'hooks/useRerender';
+
+import { ITEMS } from '../ItemData';
+import { EmojisGallery } from './EmojisGallery';
+
+export function ListWithMemoization(): JSX.Element {
+  return (
+    <ChapterWrapper title="useCallback" subtitle="List with memoization" rerender={useRerender()}>
+      <EmojisGallery items={ITEMS} />
+    </ChapterWrapper>
+  );
+}
