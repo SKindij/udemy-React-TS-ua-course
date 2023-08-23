dd3be2814c383e7b790366fdc663a9f21516d2ee
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-23 21:55:33 +0300

    feat: add significant enhancements

---
+++ mastering-react/src/6-ContextAndReducer/UseReducer/UseCounterReducer.tsx
@@ -0,0 +1,24 @@
+import { Button, ChapterWrapper, Toolbar, ValueLabel } from 'components';
+import { useReducer } from 'react';
+
+import { Action } from './Action';
+import { CounterReducer, counterReducer, INITIAL_COUNT } from './counterReducer';
+
+export function UseCounterReducer(): JSX.Element {
+  const [count, dispatch] = useReducer<CounterReducer>(counterReducer, INITIAL_COUNT);
+
+  return (
+    <ChapterWrapper title="useReducer" subtitle="Simple counter example">
+      <Toolbar>
+        <ValueLabel value={count} />
+      </Toolbar>
+
+      <Toolbar>
+        <Button text="+ 1" onClick={() => dispatch(Action.INCREMENT)} />
+        <Button text="* 2" onClick={() => dispatch(Action.DOUBLE)} />
+        <Button text="^ 2" onClick={() => dispatch(Action.POWER_2)} />
+        <Button text="Reset" onClick={() => dispatch(Action.RESET)} />
+      </Toolbar>
+    </ChapterWrapper>
+  );
+}
