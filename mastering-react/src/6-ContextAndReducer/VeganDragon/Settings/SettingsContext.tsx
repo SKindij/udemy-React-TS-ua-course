405a868f092f94e2ae2b3b69cafd6f0224947544
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-24 16:12:32 +0300

    init: set up project structure and dependencies

---
+++ mastering-react/src/6-ContextAndReducer/VeganDragon/Settings/SettingsContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, Dispatch, SetStateAction, useContext, useState } from 'react';
+import { ProviderProps } from 'types/ProviderProps';
+
+import { MAX_MOVE_DELAY, MOVE_DELAY } from '../constants';
+
+export interface SettingsContextState {
+  useAlternativeGrid: boolean;
+  setUseAlternativeGrid: Dispatch<SetStateAction<boolean>>;
+  moveDelay: number;
+  changeMoveDelay: () => void;
+}
+
+const defaultValue: any = null;
+const SettingsContext = createContext<SettingsContextState>(defaultValue);
+
+export function useSettingsContext() {
+  const contextValue = useContext(SettingsContext);
+  if (!contextValue) {
+    throw new Error('Settings context is only available under SettingsContextProvider!');
+  }
+  return contextValue;
+}
+
+export function SettingsContextProvider({ children }: ProviderProps): JSX.Element {
+  const [useAlternativeGrid, setUseAlternativeGrid] = useState<boolean>(false);
+  const [moveDelay, setMoveDelay] = useState<number>(MOVE_DELAY);
+
+  const changeMoveDelay = () => {
+    setMoveDelay((value) => {
+      return value === MAX_MOVE_DELAY ? MOVE_DELAY : value + MOVE_DELAY;
+    });
+  };
+
+  const contextValue: SettingsContextState = {
+    useAlternativeGrid,
+    setUseAlternativeGrid,
+    moveDelay,
+    changeMoveDelay,
+  };
+
+  return <SettingsContext.Provider value={contextValue}>{children}</SettingsContext.Provider>;
+}
