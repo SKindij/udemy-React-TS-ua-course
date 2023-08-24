61c340ccea7ec304d00862242e160ce1999ad108
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-24 17:09:22 +0300

    init: set up project structure and dependencies

---
+++ mastering-react/src/8-DataFetching/UsingReactQuery/Profile.tsx
@@ -0,0 +1,22 @@
+import { LoadingSpinner, PropsTable } from 'components';
+
+import { useUser } from './hooks/useUser';
+import classes from './Profile.module.css';
+
+export function Profile(): JSX.Element {
+  const { isLoading, data, error } = useUser();
+
+  if (isLoading) {
+    return (
+      <div className={classes.loading}>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className={classes.error}>Oops! Something went wrong...</div>;
+  }
+
+  return <PropsTable data={data} />;
+}
