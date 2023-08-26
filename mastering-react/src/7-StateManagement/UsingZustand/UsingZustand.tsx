5936883956558b0e92c6b08d4111c7de48b4722a
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-26 15:00:59 +0300

    feat: add entirely new capabilities

---
+++ mastering-react/src/7-StateManagement/UsingZustand/UsingZustand.tsx
@@ -0,0 +1,18 @@
+import { ChapterWrapper } from 'components';
+
+import { GridController } from './GridController';
+import { ToolbarController } from './ToolbarController';
+import classes from './UsingZustand.module.css';
+
+export function UsingZustand(): JSX.Element {
+  return (
+    <ChapterWrapper title="Zustand" subtitle="State management">
+      <div className={classes.root}>
+        <ToolbarController />
+        <div className={classes.gridContainer}>
+          <GridController />
+        </div>
+      </div>
+    </ChapterWrapper>
+  );
+}
