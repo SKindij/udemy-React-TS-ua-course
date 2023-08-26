56c2af84492bbba482d58af86f2fa580f7be39fb
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-26 15:29:03 +0300

    feat: create new data visualization component

---
+++ mastering-react/src/7-StateManagement/UsingRematch/UsingRematch.tsx
@@ -0,0 +1,22 @@
+import { ChapterWrapper } from 'components';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
+import { ToolbarController } from './ToolbarController';
+import { GridController } from './GridController';
+import classes from './UsingRematch.module.css';
+
+export function UsingRematch(): JSX.Element {
+  return (
+    <ChapterWrapper title="Rematch (Redux)" subtitle="State management">
+      <Provider store={store}>
+        <div className={classes.root}>
+          <ToolbarController />
+          <div className={classes.gridContainer}>
+            <GridController />
+          </div>
+        </div>
+      </Provider>
+    </ChapterWrapper>
+  );
+}
