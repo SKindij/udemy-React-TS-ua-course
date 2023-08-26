e4964b348d6817b202ebd4f07e7b0661ae582cd7
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-26 14:47:11 +0300

    init: create an additional files

---
+++ mastering-react/src/7-StateManagement/UsingRecoil/GridTypeButton.tsx
@@ -0,0 +1,22 @@
+import { Button } from 'components';
+import { UseState } from 'types/UseState';
+
+import { GridType } from './GridType';
+
+interface Props {
+  text: string;
+  gridType: GridType;
+  state: UseState<GridType>;
+}
+
+export function GridTypeButton({ text, gridType, state }: Props): JSX.Element {
+  const [currentGridType, setGridType] = state;
+
+  return (
+    <Button
+      text={text}
+      onClick={() => setGridType(gridType)}
+      disabled={gridType === currentGridType}
+    />
+  );
+}
