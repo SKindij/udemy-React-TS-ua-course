9e91b2714edfbdc872400e4eb4c9d26f375e5a70
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-27 15:06:26 +0300

    feat: implement new module

---
+++ mastering-react/src/8-DataFetching/UsingReactQuery/TopPanel/TopPanel.tsx
@@ -0,0 +1,21 @@
+import { Toolbar } from 'components';
+
+import { Avatar } from './Avatar';
+import { LastUpdated } from './LastUpdated';
+import { RefreshAll } from './RefreshAll';
+import { TodoCounter } from './TodoCounter';
+import { UserName } from './UserName';
+
+export function TopPanel(): JSX.Element {
+  return (
+    <Toolbar>
+      <Avatar />
+      <div style={{ flex: 1 }}>
+        <UserName />
+      </div>
+      <TodoCounter />
+      <LastUpdated />
+      <RefreshAll />
+    </Toolbar>
+  );
+}
