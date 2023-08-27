9741ec95a33d9c3581adad688ec8949a099b4c08
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-27 14:47:41 +0300

    feat: integrate third-party library

---
+++ mastering-react/src/7-StateManagement/StateManagementSection.tsx
@@ -0,0 +1,26 @@
+import { Chapter, Section } from 'components/MasterDetail';
+import { RouterPath } from 'config/RouterPath';
+
+import { UsingRecoil } from './UsingRecoil/UsingRecoil';
+import { UsingRematch } from './UsingRematch/UsingRematch';
+import { UsingZustand } from './UsingZustand/UsingZustand';
+
+export function StateManagementSection(): JSX.Element {
+  return (
+    <Section title="State management">
+      <Chapter
+        emoji="🤹🏼‍♂️"
+        title="Rematch (Redux)"
+        path={RouterPath.STATE_REMATCH}
+        element={<UsingRematch />}
+      />
+      <Chapter
+        emoji="🤹🏼‍♂️"
+        title="Zustand"
+        path={RouterPath.STATE_ZUSTAND}
+        element={<UsingZustand />}
+      />
+      <Chapter emoji="🤹🏼‍♂️" title="Recoil" path={RouterPath.STATE_RECOIL} element={<UsingRecoil />} />
+    </Section>
+  );
+}
