c08a0e92fe06ac4b3f1f4c678af1981b86c74de1
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-08-31 20:00:53 +0300

    init: create an additional files

---
+++ mastering-react/src/8-DataFetching/UsingSwr/TopPanel/RefreshAll.tsx
@@ -0,0 +1,17 @@
+import { useUserId } from '8-DataFetching/UserContext';
+import { Button } from 'components';
+import { useSWRConfig } from 'swr';
+
+import { Queries, QueryKeyFactory } from '../api/QueryKeyFactory';
+
+export function RefreshAll(): JSX.Element {
+  const userId = useUserId();
+  const { mutate: refetch } = useSWRConfig();
+
+  const handleClick = () => {
+    refetch(QueryKeyFactory[Queries.USER_DETAILS](userId));
+    refetch(QueryKeyFactory[Queries.TODO_LIST](userId));
+  };
+
+  return <Button text="Refresh all" onClick={handleClick} />;
+}
