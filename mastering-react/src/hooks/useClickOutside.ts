391de3f97bc00b01d6a365702f01a9bd0a9b811e
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-09-07 21:40:11 +0300

    init: create  necessary file

---
+++ mastering-react/src/hooks/useClickOutside.ts
@@ -0,0 +1,19 @@
+import { useCallback, useRef } from 'react';
+import { RefCallback } from 'types/RefCallback';
+
+import { useWindowEvent } from './useWindowEvent';
+
+export function useClickOutside(callback: () => void): RefCallback {
+  const elementRef = useRef<HTMLElement | null>(null);
+
+  useWindowEvent('click', (event: PointerEvent) => {
+    const path = event.composedPath();
+    if (path.every((target) => target !== elementRef.current)) {
+      callback();
+    }
+  });
+
+  return useCallback((element: HTMLElement | null) => {
+    elementRef.current = element;
+  }, []);
+}
