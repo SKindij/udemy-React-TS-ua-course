8f29026377c119ddb9e06c217d50fe4d66b93ae1
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-09-15 23:13:54 +0300

    chore: perform general project maintenance tasks

---
+++ mastering-react/src/hooks/useTimeout.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef, useState } from 'react';
+
+export interface UseTimeout {
+  reschedule: () => void;
+}
+
+export function useTimeout(callback: () => void, timeout: number | null): UseTimeout {
+  const callbackRef = useRef(callback);
+  const [dummyState, setDummyState] = useState<any>();
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (!timeout) {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => callbackRef.current(), timeout);
+
+    return () => clearTimeout(timeoutId);
+  }, [timeout, dummyState]);
+
+  return {
+    reschedule: () => setDummyState({}),
+  };
+}
