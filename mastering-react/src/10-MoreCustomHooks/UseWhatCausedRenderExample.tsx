7cad9d14ad24a48e0b426e2ab446c202b82c8e51
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-10-08 10:34:39 +0300

    feat: implement new module

---
+++ mastering-react/src/10-MoreCustomHooks/UseWhatCausedRenderExample.tsx
@@ -0,0 +1,45 @@
+import { ChapterWrapper, ColoredBlock, Toolbar } from 'components';
+import { useWhatCausedRender } from 'hooks/useWhatCausedRender';
+import { useState } from 'react';
+
+interface BadgeProps {
+  firstName: string;
+  lastName: string;
+}
+
+function Badge(props: BadgeProps): JSX.Element {
+  const { firstName, lastName } = props;
+
+  useWhatCausedRender('Badge', props);
+
+  return (
+    <ColoredBlock>
+      😑 {firstName} {lastName}
+    </ColoredBlock>
+  );
+}
+
+export function UseWhatCausedRenderExample(): JSX.Element {
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+
+  return (
+    <ChapterWrapper title="useWhatCausedRender" subtitle="More custom hooks">
+      <Toolbar>
+        <input
+          type="text"
+          placeholder="First name"
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Last name"
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+      </Toolbar>
+      <Badge firstName={firstName} lastName={lastName} />
+    </ChapterWrapper>
+  );
+}
